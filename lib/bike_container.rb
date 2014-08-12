7089f634c433f7a1efc71eb3c65d1b27ebecf1a7
kikrahau <vincent.koch@hotmail.com>
2014-08-12 19:00:38 +0100

    start from scratch

---
+++ lib/bike_container.rb
@@ -0,0 +1,36 @@
+module BikeContainer
+	DEFAULT_CAPACITY = 10
+
+def bikes
+	@bikes || = []
+end
+
+def capacity
+	@capacity || = DEFAULT_CAPACITY
+end
+
+def capacity=(value)
+	@capacity = values
+end
+
+def bike_count
+	bikes.count
+end
+
+def dock(bike)
+	#if capacity is reached, raise an exception
+	raise "Station is full" if full?
+	bikes << bike
+end
+
+def release(bike)
+	bikes.delete(bike)
+end
+
+def full?
+	bike_count == capacity
+end
+def available_bikes
+	bikes.reject {|bike| bike.broken?}
+end
+end
--- lib/bikes.rb
+++ lib/bikes.rb
@@ -1,15 +1,2 @@
 class Bike
-#the initialize method is always called when you create a new
-#class by typing Bike.new
-	def broken?
-		@broken
-	end
-
-	def break!
-		@broken = true
-	end
-
-	def fix!
-		@broken = false
-	end
 end
